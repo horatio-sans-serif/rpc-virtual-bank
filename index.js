e0a8e8a452384785f3dcf74694c17e554f35fd35
REDACTED <REDACTED_EMAIL>
2016-11-09 20:30:59 -0500

    Change export pattern

--- index.js
+++ index.js
@@ -25,126 +25,132 @@ module.exports = function ({redis, products, freeCoinsAmt, freeCoinsAfter, upgra
     `
   })
 
-  return {
-    getProductIds: function ({}) {
-      return products
-    },
+  function getProducts({}) {
+    return products
+  }
 
-    verifyIAP: function ({ platform, receipt }) {
-      if (!this.clientId)
-        return Promise.reject('authentication required')
+  function verifyIAP({ platform, receipt }) {
+    if (!this.clientId)
+      return Promise.reject('authentication required')
 
-      return new Promise((resolve, reject) => {
-        iap.verifyPayment(platform, {receipt}, (error, result) => {
-          if (error) {
-            emitter.emit('iap-rejected', this, platform, receipt, error)
-            return reject(error)
-          }
+    return new Promise((resolve, reject) => {
+      iap.verifyPayment(platform, {receipt}, (error, result) => {
+        if (error) {
+          emitter.emit('iap-rejected', this, platform, receipt, error)
+          return reject(error)
+        }
 
-          const coinsPurchased = products[result.product_id]
-          if (coinsPurchased === undefined) {
-            const error = new Error('unknown product')
-            emitter.emit('iap-rejected', this, platform, receipt, error)
-            return reject(error)
-          }
+        const coinsPurchased = products[result.product_id]
+        if (coinsPurchased === undefined) {
+          const error = new Error('unknown product')
+          emitter.emit('iap-rejected', this, platform, receipt, error)
+          return reject(error)
+        }
 
-          redis.hincrby(`users/${this.clientId}`, 'coins', coinsPurchased)
-            .then(balance => {
-              emitter.emit('iap-verified', this, platform, product_id, coinsPurchased)
-              resolve({balance})
-            })
-            .catch(error => reject(error))
-        })
+        redis.hincrby(`users/${this.clientId}`, 'coins', coinsPurchased)
+          .then(balance => {
+            emitter.emit('iap-verified', this, platform, product_id, coinsPurchased)
+            resolve({balance})
+          })
+          .catch(error => reject(error))
       })
-    },
+    })
+  }
 
-    getCoinStatus: function () {
-      if (!this.clientId)
-        return Promise.reject('authentication required')
+  function getCoinStatus() {
+    if (!this.clientId)
+      return Promise.reject('authentication required')
 
-      return redis.hmget(`users/${this.clientId}`, 'coins', 'free')
-        .then(results => {
-          const balance = Math.max(0, parseInt(results[0], 10) || 0)
-          const freeCoinsAt = Math.max(0, parseInt(results[1], 10) || 0)
-          const epoch = _.now() / 1000 | 0
-          var nextFreeCoinsAt
-          if (freeCoinsAt === 0 || freeCoinsAt + freeCoinsAfter <= epoch)
-            nextFreeCoinsAt = epoch
-          else
-            nextFreeCoinsAt = freeCoinsAt + freeCoinsAfter
-          return {balance, nextFreeCoinsAt}
-        })
-    },
+    return redis.hmget(`users/${this.clientId}`, 'coins', 'free')
+      .then(results => {
+        const balance = Math.max(0, parseInt(results[0], 10) || 0)
+        const freeCoinsAt = Math.max(0, parseInt(results[1], 10) || 0)
+        const epoch = _.now() / 1000 | 0
+        var nextFreeCoinsAt
+        if (freeCoinsAt === 0 || freeCoinsAt + freeCoinsAfter <= epoch)
+          nextFreeCoinsAt = epoch
+        else
+          nextFreeCoinsAt = freeCoinsAt + freeCoinsAfter
+        return {balance, nextFreeCoinsAt}
+      })
+  }
 
-    collectFreeCoins: function () {
-      if (!this.clientId)
-        return Promise.reject('authentication required')
+  function collectFreeCoins() {
+    if (!this.clientId)
+      return Promise.reject('authentication required')
 
-      const epoch = _.now() / 1000 | 0
+    const epoch = _.now() / 1000 | 0
 
-      return redis.hget(`users/${this.clientId}`, 'free')
-        .then(freeCoinsAt => {
-          freeCoinsAt = Math.max(0, parseInt(freeCoinsAt, 10) || 0)
-          if (freeCoinsAt === 0 || freeCoinsAt + freeCoinsAfter <= epoch) {
-            redis.pipeline()
-              .hincrby(key, 'coins', freeCoinsAmt)
-              .hset(key, 'free', epoch)
-              .exec()
-              .then(results => {
-                const balance = +results[0][1]
-                emitter.emit('free-coins-collected', this)
-                return {balance, nextFreeCoinsAt: epoch + freeCoinsAfter}
-              })
-              .catch(reject)
-          } else {
-            const error = new Error('too soon')
-            emitter.emit('free-coins-error', this, error)
-            reject(error)
-          }
-        })
-    },
+    return redis.hget(`users/${this.clientId}`, 'free')
+      .then(freeCoinsAt => {
+        freeCoinsAt = Math.max(0, parseInt(freeCoinsAt, 10) || 0)
+        if (freeCoinsAt === 0 || freeCoinsAt + freeCoinsAfter <= epoch) {
+          redis.pipeline()
+            .hincrby(key, 'coins', freeCoinsAmt)
+            .hset(key, 'free', epoch)
+            .exec()
+            .then(results => {
+              const balance = +results[0][1]
+              emitter.emit('free-coins-collected', this)
+              return {balance, nextFreeCoinsAt: epoch + freeCoinsAfter}
+            })
+            .catch(reject)
+        } else {
+          const error = new Error('too soon')
+          emitter.emit('free-coins-error', this, error)
+          reject(error)
+        }
+      })
+  }
 
-    debit: function ({amt}) {
-      if (!this.clientId)
-        return Promise.reject('authentication required')
+  function debit({amt}) {
+    if (!this.clientId)
+      return Promise.reject('authentication required')
 
-      return redis.debit(`users/${this.clientId}`, amt)
-        .then(balance => {
-          balance = +balance
-          if (isNaN(balance))
-            throw new Error('invalid-balance')
-          emitter.emit('debit', this, amt)
-          return {balance: balance}
-        })
-        .catch(error => {
-          emitter.emit('debit-error', this, amt, error)
-          throw error
-        })
-    },
+    return redis.debit(`users/${this.clientId}`, amt)
+      .then(balance => {
+        balance = +balance
+        if (isNaN(balance))
+          throw new Error('invalid-balance')
+        emitter.emit('debit', this, amt)
+        return {balance: balance}
+      })
+      .catch(error => {
+        emitter.emit('debit-error', this, amt, error)
+        throw error
+      })
+  }
 
-    // We have an old non-consumable product from an old version of the app.
-    // Allow the user to redeem a purchase thereof once for some coins.
-    // This happens if the user restores purchases in the latest version of the app.
+  // We have an old non-consumable product from an old version of the app.
+  // Allow the user to redeem a purchase thereof once for some coins.
+  // This happens if the user restores purchases in the latest version of the app.
 
-    didPreviouslyUpgrade: function ({}) {
-      if (!this.clientId)
-        return Promise.reject('authentication required')
+  function didPreviouslyUpgrade({}) {
+    if (!this.clientId)
+      return Promise.reject('authentication required')
 
-      return redis.hsetnx(`users/${this.clientId}`, 'didUpgrade', 1)
-        .then(didSet => {
-          if (!didSet)
-            throw new Error('already-upgraded')
+    return redis.hsetnx(`users/${this.clientId}`, 'didUpgrade', 1)
+      .then(didSet => {
+        if (!didSet)
+          throw new Error('already-upgraded')
 
-          return redis.hincrby(`users/${this.clientId}`, 'coins', upgradeRedeemCoins)
-        })
-        .then(balance => {
-          balance = +balance
-          if (isNaN(balance))
-            throw new Error('invalid-balance')
-          return {balance}
-        })
-    },
+        return redis.hincrby(`users/${this.clientId}`, 'coins', upgradeRedeemCoins)
+      })
+      .then(balance => {
+        balance = +balance
+        if (isNaN(balance))
+          throw new Error('invalid-balance')
+        return {balance}
+      })
+  }
 
+  return {
+    getProducts,
+    verifyIAP,
+    getCoinStatus,
+    collectFreeCoins,
+    debit,
+    didPreviouslyUpgrade,
     emitter
   }
 }
